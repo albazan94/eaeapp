d46ef2a1bab014b5e22fad03847de14cfc001e87
albazan94 <144734735+albazan94@users.noreply.github.com>
2024-01-31 22:59:12 +0100

    Update FileUpload.vue

--- resources/js/Pages/Components/FileUpload.vue
+++ resources/js/Pages/Components/FileUpload.vue
@@ -78,7 +78,7 @@ const onFileChange = (event) => {
 
     const formData = new FormData()
     formData.append('file', file)
-    const url = ''
+    const url = 'https://g1x4n98xb7.execute-api.us-east-1.amazonaws.com/dev/files'
     const config = {
         headers: {
             'Content-Type': 'text/csv'
